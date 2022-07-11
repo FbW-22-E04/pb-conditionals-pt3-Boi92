55d5a57a804dba619a291f9dd1aef1a5f866bf01
Boi Schrader <boi1992@t-online.de>
2022-07-11 16:13:11 +0200

    done for today

---
+++ solutions.js
@@ -0,0 +1,14 @@
+let aleeza = (89 + 120 + 103) / 3
+let liss = (116 + 94 + 123) /3
+
+if (aleeza > liss)
+    console.log("Aleezas Team won by", aleeza ,"to",liss,"points")
+else if (liss > aleeza)
+console.log("Liss's Team won by a", liss ,"to",aleeza,"point average")
+aleeza = (132 + 102 + 99) / 3
+if (aleeza > liss)
+    console.log("Aleezas Team won by", aleeza ,"to",liss,"points")
+else if (liss > aleeza)
+console.log("Liss's Team won by a", liss ,"to",aleeza,"point average")
+
+
